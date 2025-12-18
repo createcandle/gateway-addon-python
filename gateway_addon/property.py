9a71dc958825601590eebc331ca8458b0a542174
flatsiedatsie <unfold@gmail.com>
2025-12-18 14:49:10 +0100

    Update property.py

--- gateway_addon/property.py
+++ gateway_addon/property.py
@@ -122,7 +122,7 @@ class Property:
         """
         return self.value
 
-    def set_value(self, value, origin):
+    def set_value(self, value, meta):
         """
         Set the current value of the property.
 
